d6207d498cfaf19b0aa751c63de2d72fbc57d765
Marcello Bozzi <bozzimarcello@gmail.com>
2022-01-28 09:52:42 +0100

    Aggiunto la richiesta nome

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dimmi il tuo nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Ciao " + nome);
+
+        }
+    }
+}
